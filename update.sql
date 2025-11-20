406d4f723430195c42aa19f31340f6a3779ca6aa
Craig McKeachie <craigdmckeachie@gmail.com>
2025-11-20 14:25:26 -0500

    delete update

--- update.sql
+++ update.sql
@@ -5,7 +5,6 @@ SET first_name = 'PATTY', last_name = 'JOHNSTON'
 WHERE customer_id = 2;
 
 
-
 select * 
 from customer
 WHERE first_name = 'PATTY';
@@ -18,3 +17,5 @@ SET first_name = 'PATRICE'
 WHERE first_name = 'PATTY';
 
 SET SQL_SAFE_UPDATES=1;
+
+
